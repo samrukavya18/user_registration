4d4c0b5b4fbc494d785c97e0e2d71c757cae085f
samrukavya18 <samrukavya18@gmail.com>
2020-10-15 15:06:35 +0530

    Last Name

--- user.sh
+++ user.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
 echo " WELCOME TO USER REGISTRATION"
+read -p "Enter your First Name : " fname
+function name()
+{
+      firstname="^([A-Z]+)([A-Za-z]{2,15})$"
+      if [[ fname =~ $firstname ]]
+      then
+          echo " valid Name"
+      else
+          echo "Invalid Name"
+      fi
+}
+name $fname
+
+read -p "Enter your Last Name : " lname
+function namel()
+{
+      lastname="^([A-Z]+)([A-Za-z]{2,15})$"
+      if [[ lname =~ $lastname ]]
+      then
+          echo " valid Name"
+      else
+          echo "Invalid Name"
+      fi
+}
+namel $lname
