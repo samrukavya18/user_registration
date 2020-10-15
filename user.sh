85bd6b3db7f8e7b0ca6bef8bdb81fbdd231838c3
samrukavya18 <samrukavya18@gmail.com>
2020-10-15 14:59:41 +0530

    First Capital letter Name

--- user.sh
+++ user.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo " WELCOME TO USER REGISTRATION"
+
+read -p "Enter your First Name : " fname
+function name()
+{
+      firstname="^([A-Z]+)([A-Za-z]{2,15})$"
+      if [[ fname =~ $firstname ]]
+      then
+          echo " valid Name"
+      else
+          echo "Invalid Name"
+      fi
+}
+name $fname
